35426b711e76f60eaab0d19c0d16aa3448b000d0
Mariusz Cz <hagnar93@gmail.com>
2021-11-23 14:43:45 +0100

    Added some code into ZadanieDomowe2 class

--- src/main/java/ZadanieDomowe2.java
+++ src/main/java/ZadanieDomowe2.java
@@ -1,2 +1,28 @@
 public class ZadanieDomowe2 {
+
+    // 1. 4 zmienne typu int
+    // 2. Sprawdzamy czy suma dwóch pierwszych jest parzysta
+    // 3. Sprawdzamy czy suma dwóch pozostałych jest parzysta
+    // 4. Jeżeli dwie sumy są parzyste to wyświetl informację, że wszystkie liczby są parzyste
+
+    public static void main(String[] args) {
+        int number1 = 342423;
+        int number2 = 41232;
+        int number3 = 5422;
+        int number4 = 34246;
+
+        int firstSum = number1 + number2;
+        int secondSum = number3 + number4;
+
+        System.out.println(firstSum);
+        System.out.println(secondSum);
+
+        if (firstSum % 2 == 0 && secondSum % 2 == 0) {
+            System.out.println("Wszystkie liczby są parzyste!");
+        } else {
+            System.out.println("Nie wszystkie liczby są parzyste :(");
+        }
+    }
+
+
 }
